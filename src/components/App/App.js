840b6537502bebc99ce48a87a22368800f106592
Luke Peña <luketpena@gmail.com>
2019-12-02 15:39:35 -0600

    Project running and components on the way

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+//Import components
+import Header from '../Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
